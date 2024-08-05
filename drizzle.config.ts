136da61dc31e27a9016a94c4c20a6c512992aa49
Phan Anh <phananh12042003@gmail.com>
2024-08-05 15:04:07 -0400

    Set up NextAuth 🚀

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -3,7 +3,7 @@ export default defineConfig({
   schema: "./utils/schema.ts",
   dialect: "postgresql",
   dbCredentials: {
-    url: "postgresql://neondb_owner:9HVIrKC1uflj@ep-wandering-surf-a5zg3uxp.us-east-2.aws.neon.tech/interview-expert?sslmode=require",
+    url: process.env.NEXT_PUBLIC_DRIZZLE_DB_URL!,
   },
   verbose: true,
   strict: true,
--- utils/db.ts
+++ utils/db.ts
@@ -3,3 +3,5 @@ import { drizzle } from "drizzle-orm/neon-http";
 import * as schema from "./schema";
 const sql = neon(process.env.NEXT_PUBLIC_DRIZZLE_DB_URL!);
 const db = drizzle(sql, { schema });
+
+export { db };
