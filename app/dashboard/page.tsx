8146a349bf34a0822a537d6b94491217a6161964
Khanh Ha <nkhanhha26@gmail.com>
2024-08-07 01:20:28 -0400

    added create form modal🚀

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,24 +1,37 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
-import HumanInterviewRoom from "../human/page";
+import { Button } from "@/components/ui/button";
+import AIHumanForm from "@/components/ai-or-human-form";
+import dynamic from "next/dynamic";
+
+const HumanInterviewRoom = dynamic(() => import("../human/page"), {
+  ssr: false,
+});
 
 const Dashboard = () => {
+  const [openDialog, setOpenDialog] = useState(false);
+
   return (
     <div className="justify-center items-center flex flex-col p-5 space-y-7">
-      <div className="flex flex-row">
-        <div className="flex flex-col justify-center items-center text-center p-6 h-fit">
-          <h2 className="text-5xl font-extrabold opacity-85">Prepare for</h2>
-          <h1 className="text-6xl font-extrabold p-5 bg-gradient-to-r from-yellow-400 to-orange-500 bg-clip-text text-transparent">
-            Mock Interview
-          </h1>
-          <p className="text-gray-500 dark:text-gray-400 mt-5">
-            It's so easy. Just one click and you are on the way to practice!
-          </p>
-        </div>
+      <div className="flex flex-col justify-center items-center p-6 h-fit w-1/2">
+        <h2 className="text-5xl font-extrabold opacity-85">Prepare for</h2>
+        <h1 className="text-6xl font-extrabold p-5 bg-gradient-to-r from-yellow-400 to-orange-500 bg-clip-text text-transparent">
+          Mock Interview
+        </h1>
+        <p className="text-gray-500 dark:text-gray-400 mt-5">
+          It's so easy. Just one click and you are on the way to practice!
+        </p>
+      </div>
+      <div className="justify-center items-center flex">
+        <Button onClick={() => setOpenDialog(true)}>Create Room</Button>
+      </div>
+      <div className="justify-center items-center flex">
+        <h2 className="text-xl font-bold text-gray-500">OR</h2>
       </div>
       <div className="justify-center items-center flex w-screen">
         <Tabs defaultValue="account">
-          <div className="justify-center items-center flex ">
+          <div className="justify-center items-center flex">
             <TabsList className="grid w-80 h-auto grid-cols-2">
               <TabsTrigger value="ai">AI Interview</TabsTrigger>
               <TabsTrigger value="human">Human Interview</TabsTrigger>
@@ -32,6 +45,9 @@ const Dashboard = () => {
           </TabsContent>
         </Tabs>
       </div>
+      {openDialog && (
+        <AIHumanForm openDialog={openDialog} setOpenDialog={setOpenDialog} />
+      )}
     </div>
   );
 };
--- app/human/page.tsx
+++ app/human/page.tsx
@@ -1,49 +1,5 @@
-import { Button } from "@/components/ui/button";
-import { db } from "@/utils/db";
-import Link from "next/link";
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
-import { Room } from "@/utils/schema";
-import { GithubIcon } from "lucide-react";
 import { getHumanRooms } from "@/data-access/human-rooms";
-import { LanguagesList } from "@/components/languages-list";
-import { splitLanguages } from "@/lib/utils";
-
-function RoomCard({ room }: { room: Room }) {
-  return (
-    <Card className="shadow-md hover:shadow-lg hover:translate-y-2 transition-all cursor-pointer">
-      <CardHeader>
-        <CardTitle>{room.name}</CardTitle>
-        <CardDescription>{room.description}</CardDescription>
-      </CardHeader>
-      <CardContent className="flex flex-col gap-4">
-        <LanguagesList languages={splitLanguages(room?.language || "")} />
-        {room.githubRepo && (
-          <Link
-            href={room.githubRepo}
-            className="flex items-center gap-2"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <GithubIcon></GithubIcon>
-            GitHub Project
-          </Link>
-        )}
-      </CardContent>
-      <CardFooter>
-        <Button asChild>
-          <Link href={`/human-rooms/${room.id}`}>Join Room</Link>
-        </Button>
-      </CardFooter>
-    </Card>
-  );
-}
+import RoomCard from "@/components/roomcard";
 
 export default async function HumanInterviewRoom() {
   const rooms = await getHumanRooms();
@@ -51,9 +7,6 @@ export default async function HumanInterviewRoom() {
     <main className="min-h-screen p-16">
       <div className="flex justify-between w-full items-center mb-12">
         <h1 className="text-4xl font-semibold">Find Interview Room</h1>
-        <Button asChild>
-          <Link href="/human/create-room">Create Room</Link>
-        </Button>
       </div>
       <div className="grid grid-cols-3 gap-4">
         {rooms.map((room) => {
